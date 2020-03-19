2ea75cf8e88ac33f613e1a82352e9b33b6c29a13
Pat Palmerston <patpalmerston@gmail.com>
2020-03-19 10:33:42 -0700

    social solution

--- guided/social.py
+++ guided/social.py
@@ -1,4 +1,5 @@
 import random
+from util import Stack, Queue  # These may come in handy
 
 class User:
     def __init__(self, name):
@@ -15,12 +16,15 @@ class SocialGraph:
         Creates a bi-directional friendship
         """
         if user_id == friend_id:
-            print("WARNING: You cannot be friends with yourself")
+            # print("WARNING: You cannot be friends with yourself")
+            return False
         elif friend_id in self.friendships[user_id] or user_id in self.friendships[friend_id]:
-            print("WARNING: Friendship already exists")
+            # print("WARNING: Friendship already exists")
+            return False
         else:
             self.friendships[user_id].add(friend_id)
             self.friendships[friend_id].add(user_id)
+            return True
 
     def add_user(self, name):
         """
@@ -44,7 +48,6 @@ class SocialGraph:
         self.last_id = 0
         self.users = {}
         self.friendships = {}
-        # !!!! IMPLEMENT ME
 
         # Add users
         # Write a for loop that calls create user the right amount of times
@@ -70,6 +73,34 @@ class SocialGraph:
             self.add_friendship(friendship[0], friendship[1])
 
 
+    def populate_graph_linear(self, num_users, avg_friendships):
+        # Reset graph
+        self.last_id = 0
+        self.users = {}
+        self.friendships = {}
+
+        # Add users
+        # Write a for loop that calls create user the right amount of times
+        for i in range(num_users):
+            self.add_user(f"User {i+1}")
+
+        target_friendships = num_users * avg_friendships
+        total_friendships = 0
+        collisions = 0
+        while total_friendships < target_friendships:
+            # Pick a random user
+            user_id = random.randint(1, num_users)
+            # Pick another random user
+            friend_id = random.randint(1, num_users)
+            # Try to create the friendship
+            if self.add_friendship(user_id, friend_id):
+                # If it works, increment a counter
+                total_friendships += 2
+            else:
+                # If not, try again
+                collisions += 1
+        print(f"NUM COLLISIONS: {collisions}")
+
     def get_all_social_paths(self, user_id):
         """
         Takes a user's user_id as an argument
@@ -79,14 +110,61 @@ class SocialGraph:
 
         The key is the friend's ID and the value is the path.
         """
+        # Create a queue
+        q = Queue()
+        # Enqueue a path to the starting user_id
+        q.enqueue([user_id])
         visited = {}  # Note that this is a dictionary, not a set
-        # !!!! IMPLEMENT ME
+        # While the queue is not empty...
+        while q.size() > 0:
+            # Dequeue the first path
+            path = q.dequeue()
+            # Grab the last id from the path
+            current_id = path[-1]
+            # Check if it's been visited
+            # If not...
+            if current_id not in visited:
+                # Add it to visited along with the path
+                visited[current_id] = path
+                # Enqueue the path to each friend to the queue
+                for friend_id in self.friendships[current_id]:
+                    # Copy the path
+                    path_copy = path.copy()
+                    # Append each neighbor
+                    path_copy.append(friend_id)
+                    # Enqueue
+                    q.enqueue(path_copy)
         return visited
 
+import time
 
 if __name__ == '__main__':
-    sg = SocialGraph()
-    sg.populate_graph(10, 2)
-    print(sg.friendships)
+    # sg = SocialGraph()
+    # sg.populate_graph(1000, 5)
+    # print(sg.friendships)
     # connections = sg.get_all_social_paths(1)
     # print(connections)
+    # print(len(connections) / 1000)
+    # total = 0
+    # for path in connections.values():
+    #     total += len(path)
+    # print(f"Avg degrees of separation = {total / len(connections) - 1}")
+    num_users = 2000
+    avg_friendships = 1999
+    sg = SocialGraph()
+    start_time = time.time()
+    sg.populate_graph(num_users, avg_friendships)
+    end_time = time.time()
+    print("\n\n-----")
+    print(f"Quadratic populate: {end_time - start_time} seconds")
+    print("-----\n\n")
+    sg = SocialGraph()
+    start_time = time.time()
+    sg.populate_graph_linear(num_users, avg_friendships)
+    end_time = time.time()
+    print(f"Linear populate: {end_time - start_time} seconds")
+
+
+
+
+
